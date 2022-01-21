6ac2e8589d7a3ff44ef3020e0db563e46d451547
Rezapu Obed <obed.ubaidillah@gmail.com>
2022-01-21 19:26:00 +0700

    stops player when interacting with object

--- src/world/InteractiveObject.gd
+++ src/world/InteractiveObject.gd
@@ -4,31 +4,42 @@ var mouse_here := false
 onready var explanation := get_node("CanvasLayer")
 onready var outline := $Outline
 
+
 func _ready():
 	outline.visible=false
 	explanation.set_process(false)
 	$CanvasLayer/TextureRect.set_stretch_mode(6)
 	explanation.get_node("TextureRect").visible=false
-	pass 
+
 
 func _process(delta):
 	if mouse_here and Input.is_action_just_pressed("click") :
 		explanation.get_node("TextureRect").visible=true
+		turn_off_player()
 	if mouse_here == false :
-		
 		if Input.is_action_just_pressed("click") :
 			explanation.get_node("TextureRect").visible=false
-	pass
+			turn_on_player()
 
 
 func _on_Area2D_mouse_entered():
-	print('a')
 	explanation.set_process(true)
 	outline.visible=true
 	mouse_here = true
-	pass 
+
 
 func _on_Area2D_mouse_exited():
 	outline.visible = false
 	mouse_here = false
-	pass 
+
+
+func turn_on_player():
+	var player = get_tree().get_root().find_node("Player", true, false)
+	if player:
+		player.set_active(true)
+		
+
+func turn_off_player():
+	var player = get_tree().get_root().find_node("Player", true, false)
+	if player:
+		player.set_active(false)
