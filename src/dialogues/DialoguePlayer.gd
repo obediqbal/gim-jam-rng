85455f9e3c98964c8a9635750e7867fb49507ab7
Rezapu Obed <obed.ubaidillah@gmail.com>
2022-01-26 12:34:01 +0700

    add choice box to dialogue

--- src/dialogues/DialoguePlayer.gd
+++ src/dialogues/DialoguePlayer.gd
@@ -19,8 +19,6 @@ func play(current_scene, current_npc, current_room):
 	scene = current_scene
 	npc = current_npc
 	room = current_room
-	print(current_room)
-	print('a')
 	
 	dialogues = load_dialogues(scene)
 	is_dialogue_active = true
@@ -44,14 +42,49 @@ func _input(event):
 func next_line():
 	current_dialogue_id += 1
 	
+	
 	if current_dialogue_id >= len(dialogues[room][npc]):
 		$Timer.start()
 		$Dialogue.visible = false
 		turn_on_player()
 		return
-	
-	$Dialogue/Name.text = dialogues[room][npc][current_dialogue_id]["name"]	
-	$Dialogue/Message.text = dialogues[room][npc][current_dialogue_id]["text"]
+		
+	$Dialogue/Options/Choice1.visible = false
+	$Dialogue/Options/Choice2.visible = false
+	$Dialogue/Options/Choice3.visible = false
+		
+	var current_dialogue = dialogues[room][npc][current_dialogue_id]
+		
+	# atribut "name" kosong maka gunakan Dialogue Box NoName
+	if current_dialogue.has("name") and current_dialogue["name"]=="":
+		$Dialogue/WithName.visible = false
+		$Dialogue/NoName.visible = true
+	# tidak punya atribut "name" maka gunakan "name" sebelumnya
+	else:
+		if current_dialogue.has("name"):
+			$Dialogue/WithName/Name.text = current_dialogue["name"]
+		$Dialogue/NoName.visible = false
+		$Dialogue/WithName.visible = true
+		
+	if current_dialogue.has("decision"):
+		var options = current_dialogue["option"]
+		
+		if options.size() >= 1:
+			$Dialogue/Options/Choice1.visible = true
+			$Dialogue/Options/Choice1/Option.text = options[0]
+		if options.size() >= 2:
+			$Dialogue/Options/Choice2.visible = true
+			$Dialogue/Options/Choice2/Option.text = options[1]
+		if options.size() >= 3:
+			$Dialogue/Options/Choice3.visible = true
+			$Dialogue/Options/Choice3/Option.text = options[2]
+			
+		if current_dialogue["decision"]:
+			pass
+		else:
+			pass
+	if current_dialogue.has("text"):
+		$Dialogue/Message.text = current_dialogue["text"]
 
 func load_dialogues(level):
 	var file = File.new()
