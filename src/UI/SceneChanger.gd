23cf40a2e9d1a8a4c5f7422afebfb1e07cb4d259
Rezapu Obed <obed.ubaidillah@gmail.com>
2022-02-01 11:57:07 +0700

    added SceneChanger

---
+++ src/UI/SceneChanger.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+onready var AnimationN = $Control/AnimationPlayer
+var scene : String
+
+
+func change_scene(new_scene, anim):
+	scene = new_scene
+	AnimationN.play(anim)
+	
+
+func _new_scene():
+	get_tree().change_scene(scene)
+	print('changed')
