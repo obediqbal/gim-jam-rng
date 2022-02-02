8496aad8fb577293c159adaa0080de27e5a0524d
Rezapu Obed <obed.ubaidillah@gmail.com>
2022-02-02 23:37:57 +0700

    added transition from porch scene into porch

--- src/UI/StartScreen.gd
+++ src/UI/StartScreen.gd
@@ -2,4 +2,4 @@ extends Control
 
 
 func _on_StartGameButton_pressed():
-	SceneChanger.change_scene('res://src/level/intro.tscn', 'fade')
+	SceneChanger.change_scene('res://src/level/0/porch_scene.tscn', 'fade')
---
+++ src/level/0/porch_scene_dialogue.gd
@@ -0,0 +1,7 @@
+extends "res://src/dialogues/DialoguePlayer.gd"
+
+
+func next_line():
+	.next_line()
+	if current_dialogue_id >= len(current_parent):
+		SceneChanger.change_scene("res://src/level/0/porch.tscn", "fade")
