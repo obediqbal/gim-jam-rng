2d89dc9362f710a9d9929bc76b8f04aaef637229
Rezapu Obed <obed.ubaidillah@gmail.com>
2022-02-05 00:35:26 +0700

    removed placeholders

--- src/actors/Player/Player.gd
+++ src/actors/Player/Player.gd
@@ -4,12 +4,8 @@ extends KinematicBody2D
 var speed := 200.0
 
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 func set_active(active):
 	set_physics_process(active)
 	set_process(active)
 	set_process_input(active)
+
