5fa3850e9ebd9f03c4b899ae2660b439c3a3f8da
Rezapu Obed <obed.ubaidillah@gmail.com>
2022-02-05 00:40:01 +0700

    added _movePlayer() with 3s loading screen

--- src/UI/SceneChanger.gd
+++ src/UI/SceneChanger.gd
@@ -2,13 +2,20 @@ extends CanvasLayer
 
 onready var AnimationN = $Control/AnimationPlayer
 var scene : String
+var start_pos : Vector2
 
 
-func change_scene(new_scene, anim):
+func change_scene(new_scene, anim, new_start_pos=null):
 	scene = new_scene
-	AnimationN.play(anim)
+	if new_start_pos!=null:
+		start_pos = new_start_pos
+	AnimationN.play(anim)	
 	
 
 func _new_scene():
 	get_tree().change_scene(scene)
-	print('changed')
+	
+	
+func _move_player():
+	if get_parent().get_child(1).has_node('Player'):
+		get_parent().get_child(1).get_node('Player').position = start_pos
