4e095f6c7bcb5ec0f6265e10f5fe995bf4184768
Rezapu Obed <obed.ubaidillah@gmail.com>
2022-02-06 23:31:24 +0700

    i dont remember what i did

--- src/UI/StartScreen.gd
+++ src/UI/StartScreen.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _on_StartGameButton_pressed():
 	SceneChanger.change_scene('res://src/level/0/porch_scene.tscn', 'fade')
+
+
+func _on_CreditsGameButton_pressed():
+	SceneChanger.change_scene('res://src/world/credits.tscn', 'fade')
