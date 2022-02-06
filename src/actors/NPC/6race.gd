d88ba415b2a47d0c91f2c99f144a80068a05ff8e
Rezapu Obed <obed.ubaidillah@gmail.com>
2022-02-06 15:14:20 +0700

    distort for grace and john

--- src/actors/NPC/6race.gd
+++ src/actors/NPC/6race.gd
@@ -4,3 +4,5 @@ extends "NPC.gd"
 func _ready():
 	npc = "Grace"
 	$DialoguePlayer/Dialogue.visible = false
+	if level == 3:
+		$DistortAnimation.visible = false
--- src/actors/NPC/john.gd
+++ src/actors/NPC/john.gd
@@ -4,3 +4,5 @@ extends "NPC.gd"
 func _ready():
 	npc = "John"
 	$DialoguePlayer/Dialogue.visible = false
+	if level == 3:
+		$DistortAnimation.visible = false
